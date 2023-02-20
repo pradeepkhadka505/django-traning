467db2c44577ad2d7ef2baf0282d1d16115905b1
pradeep khadka <pradeepkhadka505@gmail.com>
2023-02-20 13:33:14 +0545

    url and its working mechanism

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
 
     path('', views.onfunctioncall, name="index"),
-    path('about', views.about_function, name="about")
+    path('about', views.about_function, name="about"),
+    path('add/<int:a>/<int:b>', views.add, name="add"),
+    path('intro/<str:name>/<int:age>', views.intro, name="intro")
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,12 @@ def onfunctioncall(request):
 
 def about_function(request):
     return HttpResponse("this is about ")
+
+def add(request, a, b):
+    return HttpResponse( a + b)
+
+def intro(request, name, age):
+
+    my_dict = {"name": name, "age": age}
+    
+    return JsonResponse(my_dict)
