735c676b81f3a152fee8fd9b0ccc349f0973f543
pradeep khadka <pradeepkhadka505@gmail.com>
2023-02-20 16:19:09 +0545

    idea related to extend file, block title, block content as well as how to loop list and performing and checking condition like if else etc are acted

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', views.onfunctioncall, name="index"),
     path('about', views.about_function, name="about"),
     path('add/<int:a>/<int:b>', views.add, name="add"),
-    path('intro/<str:name>/<int:age>', views.intro, name="intro")
+    path('intro/<str:name>/<int:age>', views.intro, name="intro"),
+    path('myfirstpage', views.myfirstpage, name="myfirstpage"),
+    path('mysecondpage', views.mysecondpage, name="mysecondpage"),
+    path('mythirdpage', views.mythirdpage, name="mythirdpage"),
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -16,3 +16,31 @@ def intro(request, name, age):
     my_dict = {"name": name, "age": age}
     
     return JsonResponse(my_dict)
+
+def myfirstpage(request):
+    return render(request, 'index.html')
+
+def mysecondpage(request):
+    return render(request, 'second.html')
+
+def mythirdpage(request):
+
+    var = "Helloo world"
+    name = "pradeep Khadka"
+    greeting = "Hey, How R you ??"
+    fruits = ["apple","banana", "orange", "pine-apple", "grapes"]
+    num1, num2 = 100, 15
+    ans = num1 > num2
+    my_dict = {
+        "var": var, 
+        "name": name, 
+        "msg" : greeting, 
+        "myfruits" : fruits,
+        "num1": num1, 
+        "num2": num2, 
+        "ans": ans}
+    
+
+
+    return render(request, 'third.html', my_dict)
+
